190232246f91af3f7b4c634a5c0bd87e427b4772
seyeong <park-s@dym.jp>
2023-06-27 15:15:33 +0900

    ログイン画面フレーム作成

--- front/components/Auth/LoginBox.tsx
+++ front/components/Auth/LoginBox.tsx
@@ -8,21 +8,21 @@ const LoginBox = () => {
             </a>
             <div className="w-full bg-[rgb(0,81,203)] rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sign in to your account
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-white">
+                    ログインしてください
                 </h1>
                 <form className="space-y-4 md:space-y-6" action="#">
                 <div>
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                        email
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-white dark:text-white">
+                        メールアドレス
                     </label>
                     <input type="email" name="email" id="email"
                          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                          placeholder="name@company.com" required=""/>
                 </div>
                 <div>
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Password
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-white dark:text-white">
+                        パスワード
                     </label>
                     <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                 </div>
@@ -32,13 +32,13 @@ const LoginBox = () => {
                             <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"/>
                         </div>
                     <div className="ml-3 text-sm">
-                        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                        <label htmlFor="remember" className="text-gray-800 dark:text-gray-300">ログイン情報保持</label>
                     </div>
                 </div>
                 <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                 </div>
-                  <button type="submit" className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  <button type="submit" className="w-full text-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                <p className="text-sm font-light dark:text-gray-400">
                   Don’t have an account yet?
                   <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                 </p>
