38c49309f07a024f12b65f6698a476b63e0acbf7
seyeong <park-s@dym.jp>
2023-06-29 15:38:57 +0900

    型の形更新

--- front/components/Dashbord/AdminLayout.tsx
+++ front/components/Dashbord/AdminLayout.tsx
@@ -12,6 +12,7 @@ export type LoginUserProfileType = {
 export default function AdminLayout(props: {children: React.ReactNode}) {
     const session = useSession();
     const user: LoginUserProfileType = { name: String(session.data?.user?.email), image: "/people.jpg", isAdmin: false }
+    console.log(user.name)
     return (
         <div className={"min-h-screen bg-[rgb(229,229,229)]"}>
             <div className={"flex h-[8vh]"}>
@@ -24,7 +25,7 @@ export default function AdminLayout(props: {children: React.ReactNode}) {
                         <SideBarDetox />
                     </div>
                     <div className={"h-[7vh] flex items-center justify-center border-t border-white"}>
-                        <UserProfile name={user.name === "undefined" ? "no Login" : user.name} image={typeof user.image === "undefined" ? "" : user.image} />
+                        <UserProfile name={user.name === "undefined" || user.name === "null"  ? "no Login" : user.name} image={typeof user.image === "undefined" ? "" : user.image} />
                     </div>
                 </div>
                 <div className={"w-4/5 h-full text-center overflow-y-scroll bg-[rgb(229,229,229)] rounded-tl-2xl"}>
